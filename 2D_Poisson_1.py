8f711de3e5709234f56f17645b554a040e17fd72
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2023-03-13 01:29:06 -0700

    Create 2D_Poisson_1.py

---
+++ 2D_Poisson_1.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the grid size and spacing
+N = 50  # Number of grid points in each direction
+L = 1.0  # Length of the square domain
+h = L / (N - 1)  # Spacing between grid points
+
+# Define the boundary conditions and source term
+u_top = np.zeros(N)
+u_bottom = np.zeros(N)
+u_left = np.zeros(N)
+u_right = np.zeros(N)
+f = np.zeros((N, N))
+f[N//2, N//2] = 1.0/h**2
+
+# Define the coefficient matrix and the right-hand side vector
+A = np.zeros((N*N, N*N))
+b = np.zeros(N*N)
+
+for i in range(N):
+    for j in range(N):
+        k = i*N + j  # Index of the unknown u_ij
+        if i == 0:
+            A[k, k] = 1.0
+            b[k] = u_top[j]
+        elif i == N-1:
+            A[k, k] = 1.0
+            b[k] = u_bottom[j]
+        elif j == 0:
+            A[k, k] = 1.0
+            b[k] = u_left[i]
+        elif j == N-1:
+            A[k, k] = 1.0
+            b[k] = u_right[i]
+        else:
+            A[k, k] = -4.0
+            A[k, k-1] = 1.0
+            A[k, k+1] = 1.0
+            A[k, k-N] = 1.0
+            A[k, k+N] = 1.0
+            b[k] = f[i,j]
+
+# Solve the linear system
+u = np.linalg.solve(A, b)
+
+# Reshape the solution vector into a 2D array
+U = u.reshape((N, N))
+
+# Plot the solution
+x = np.linspace(0, L, N)
+y = np.linspace(0, L, N)
+X, Y = np.meshgrid(x, y)
+plt.contourf(X, Y, U)
+plt.colorbar()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('2D Poisson equation solution')
+plt.show()
