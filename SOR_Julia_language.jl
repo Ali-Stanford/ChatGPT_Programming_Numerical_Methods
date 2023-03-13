76708b507ff792417cb244326850a2926bd83b89
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2023-03-13 01:34:16 -0700

    Create SOR_Julia_language.jl

---
+++ SOR_Julia_language.jl
@@ -0,0 +1,41 @@
+# Successive over-relaxation (SOR) method for solving a linear system of equations
+# Ax = b
+# Example: solve 4x1 + x2 + x3 = 1, x1 + 4x2 + x3 = 2, x1 + x2 + 4x3 = 3
+# using SOR method with omega = 1.5 and tolerance = 1e-6
+
+# Set up the system of equations
+A = [4 1 1; 1 4 1; 1 1 4]
+b = [1; 2; 3]
+
+# Set up initial guess
+x = [0; 0; 0]
+
+# Set up relaxation parameter and tolerance
+omega = 1.5
+tol = 1e-6
+
+# Set up maximum number of iterations
+max_iter = 1000
+
+# Solve the system of equations using SOR method
+for iter_count = 1:max_iter
+    old_x = copy(x)
+    for i = 1:length(x)
+        sigma = 0
+        for j = 1:length(x)
+            if j != i
+                sigma += A[i,j]*x[j]
+            end
+        end
+        x[i] = (1 - omega)*x[i] + (omega/A[i,i])*(b[i] - sigma)
+    end
+    residual = norm(b - A*x)
+    if residual < tol
+        break
+    end
+end
+
+# Display the solution and number of iterations
+println("Solution:")
+println(x)
+println("Number of iterations: ", iter_count)
